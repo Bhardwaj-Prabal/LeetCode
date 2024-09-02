9a60af1c11b4da84b0d784332d1055e88894bec2
Prabal Bhardwaj <125324867+Bhardwaj-Prabal@users.noreply.github.com>
2024-09-02 09:55:47 +0530

    Time: 99 ms (43.59%) | Memory: 77.1 MB (8.71%) - LeetSync

--- 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.cpp
+++ 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.cpp
@@ -1,39 +1,15 @@
 class Solution {
 public:
     int chalkReplacer(vector<int>& chalk, int k) {
-        int n=chalk.size();
-        vector<long long > sum(n,0);
-        sum[0]=chalk[0];
-        for(int i=1;i<n;i++){
-            sum[i]= sum[i-1] + chalk[i];
-        }
+        long long accSum = accumulate(chalk.begin(), chalk.end(), 0LL);
+        k %= accSum; 
 
-        long long  temp=sum[n-1];
-
-        if(temp<k){
-            while(temp<k){
-                k -= temp;
+        for (int i = 0; i < chalk.size(); ++i) {
+            if (chalk[i] > k) {
+                return i;  
             }
+            k -= chalk[i];
         }
-        int i=0;
-        
-        if(n==1) return 0;
-
-        while(k!=0){
-            i= i% n;
-            
-            k = k- chalk[i];
-            if(k<0) return i%n;
-            else{
-                i++;
-            }
-
-            
-
-        }
-
-        return i%n;
-
-
+        return -1;  
     }
 };
